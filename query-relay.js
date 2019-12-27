42818e636ff5a4e1547ca0848ff8f04c9b8bff95
Nikos Aghazarian <odst46@gmail.com>
2019-12-27 13:17:16 -0800

    added ServerAuth file(git ignored)

--- query-relay.js
+++ query-relay.js
@@ -1,10 +1,21 @@
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const mysql = require('mysql');
+const ServerAuth = require('./ServerAuth.json')
+
 
 export class QueryRelay {
     
     constructor() {}
 
-
+    DbQuery(query, queryCallback) {
+        const connection = mysql.createConnection({
+            host: ServerAuth.host,
+            user: ServerAuth.user,
+            password: ServerAuth.password,
+        });
+        connection.connect();
+        connection.query(query, queryCallback);
+        connection.end();
+    }
 }
